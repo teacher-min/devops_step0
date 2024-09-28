e7d93bb42e9e096dd4cdfad95c855f971a1445ae
teacher-min <minkt78@gmail.com>
2024-09-28 15:09:49 +0900

    a

---
+++ src/test/java/ac/kr/koreatech/devops_step0/DevopsStep0ApplicationTests.java
@@ -0,0 +1,13 @@
+package ac.kr.koreatech.devops_step0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsStep0ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
