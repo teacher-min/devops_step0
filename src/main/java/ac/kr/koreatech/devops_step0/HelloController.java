f22956d8334b2ae1c02cfb213fe3721497043699
teacher-min <minkt78@gmail.com>
2024-09-29 12:33:23 +0900

    c

--- src/main/java/ac/kr/koreatech/devops_step0/HelloController.java
+++ src/main/java/ac/kr/koreatech/devops_step0/HelloController.java
@@ -1,27 +1,14 @@
 package ac.kr.koreatech.devops_step0;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-
 
 @Controller
 public class HelloController {
-
-  @Value("${secrets.SERVER_SSH_KEY}")
-  private String key;
 
   @GetMapping("/")
   public String index() {
     return "index.html";
   }
 
-  @GetMapping("/key")
-  @ResponseBody
-  public String getMethodName() {
-      return key;
-  }
-  
 }
